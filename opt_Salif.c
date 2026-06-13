ef6904aef34f4ee332392879b85fbc42afe47b28
Lesalf <salif.sarr@saintjeaningenieur.org>
2026-06-13 23:58:34 +0100

    ajout de l'option U

--- opt_Salif.c
+++ opt_Salif.c
@@ -164,7 +164,102 @@ void option_g(char *grp) {
 
     closedir(proc);
 }
-void option_U()            { /* TODO */ }
+
+/* ========================= */
+/* OPTION -U */
+/* ========================= */
+
+void option_U() {
+
+    printf("Affichage des sockets Unix...\n");
+
+    DIR *dossier_proc;
+    struct dirent *entree;
+
+    char chemin[512];
+    char lien[512];
+    char nom_process[256];
+
+    dossier_proc = opendir("/proc");
+
+    if (dossier_proc == NULL) {
+        printf("Erreur ouverture /proc\n");
+        return;
+    }
+
+    while ((entree = readdir(dossier_proc)) != NULL) {
+
+        int pid = atoi(entree->d_name);
+
+        if (pid <= 0)
+            continue;
+
+        snprintf(chemin,
+                 sizeof(chemin),
+                 "/proc/%d/comm",
+                 pid);
+
+        FILE *f = fopen(chemin, "r");
+
+        if (f == NULL)
+            continue;
+
+        fgets(nom_process,
+              sizeof(nom_process),
+              f);
+
+        fclose(f);
+
+        nom_process[strcspn(nom_process, "\n")] = 0;
+
+        snprintf(chemin,
+                 sizeof(chemin),
+                 "/proc/%d/fd",
+                 pid);
+
+        DIR *dossier_fd = opendir(chemin);
+
+        if (dossier_fd == NULL)
+            continue;
+
+        struct dirent *fd_entree;
+
+        while ((fd_entree = readdir(dossier_fd)) != NULL) {
+
+            if (fd_entree->d_name[0] == '.')
+                continue;
+
+            snprintf(chemin,
+                     sizeof(chemin),
+                     "/proc/%d/fd/%s",
+                     pid,
+                     fd_entree->d_name);
+
+            int len = readlink(chemin,
+                               lien,
+                               sizeof(lien) - 1);
+
+            if (len == -1)
+                continue;
+
+            lien[len] = '\0';
+
+            if (strncmp(lien, "socket:", 7) == 0) {
+
+                printf("%-20s %-6d %-6s %-40s\n",
+                       nom_process,
+                       pid,
+                       fd_entree->d_name,
+                       lien);
+            }
+        }
+
+        closedir(dossier_fd);
+    }
+
+    closedir(dossier_proc);
+}
+
 void option_s(char *proto) { /* TODO */ }
 void option_X()            { /* TODO */ }
 
