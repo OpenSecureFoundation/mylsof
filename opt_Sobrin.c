4f891b348badedd5b487a1f4907962a411cdc660
sobrin <sobrinngassa16@icloud.com>
2026-06-14 22:28:26 +0100

    implementer des fonvtions dans opt_Sobrin.c

--- opt_Sobrin.c
+++ opt_Sobrin.c
@@ -6,16 +6,8 @@
 #include "options.h"
 
 /* Sobrin écrit ton code ici — options : -t -a +D -o -K */
+}
 
-void option_t()               { /* TODO */ }
-void option_a()               { /* TODO */ }
-void option_D_plus(char *dir) { /* TODO */ }
-void option_o()               { /* TODO */ }
-void option_K()               { /* TODO */ }
-
-int gerer_options_Sobrin(int argc, char *argv[]) {
-    /* Sobrin ajoute tes if ici */
-    return 0;
 }
 /* ================================================================
 **  opt_Sobrin.c — Options : -l  -h  -o  -z
